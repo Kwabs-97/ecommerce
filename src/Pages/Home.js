97393edf2fbfe8c19e094e6c5931d88deb530aac
Kwabs-97 <khobewayne@gmail.com>
2023-09-13 22:14:52 +0000

    counter done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,21 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "../Styles/Home.module.css";
 import mainItem from "../assets/images/image-product-1.jpg";
 
 import { GrFormPrevious, GrFormNext } from "react-icons/gr";
 
 const Home = () => {
+  const [counter, setCounter] = useState(0);
+
+  function decrementCounter() {
+    setCounter((prevCounter) => (prevCounter -= 1));
+  }
+
+  function incrementCounter() {
+    setCounter((nextCounter) => (nextCounter += 1));
+  }
   return (
     <main className={styles.main}>
       <div className={styles.mainContainer}>
@@ -41,6 +50,14 @@ const Home = () => {
             </div>
             <h5>$250.00</h5>
           </div>
+
+          <section className={styles.counterContainer}>
+            <div className={styles.counter}>
+              <button onClick={decrementCounter}>-</button>
+              <span>{counter}</span>
+              <button onClick={incrementCounter}>+</button>
+            </div>
+          </section>
         </aside>
       </div>
     </main>
