f1e01238222f3e02c2eaea91e5b4a2b03533065b
Kwabs-97 <khobewayne@gmail.com>
2023-09-13 16:17:04 +0000

    home structure mobile first design

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,20 @@
-import React from 'react'
+/** @format */
+
+import React from "react";
+import styles from "../Styles/Home.module.css";
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <main className={styles.main}>
+      <div className={styles.mainContainer}>
+        <div className={styles.imgContainer}>
+          <div className={styles.img}></div>
+          <section className={styles.subImg}></section>
+        </div>
+        <aside className={styles.description}></aside>
+      </div>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
