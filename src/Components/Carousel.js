ee42e6d98330e447d7c9a9b18263c1a2542bdf47
Kwabs-97 <khobewayne@gmail.com>
2023-09-19 08:39:04 +0000

    carousel mobile loop

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,6 +1,8 @@
 /** @format */
 
 import React, { useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
 
 import product1 from "../assets/images/image-product-1.jpg";
 import product2 from "../assets/images/image-product-2.jpg";
@@ -29,35 +31,45 @@ const data = [
 ];
 const Carousel = () => {
   const [slideCount, setSlideCount] = useState(0);
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()]);
 
-  const nextSlide = () => {
-    setSlideCount(slideCount === data.length - 1 ? 0 : slideCount + 1);
-  };
-  const previousSlide = () => {
-    setSlideCount(slideCount === 0 ? data.length - 1 : slideCount - 1);
-  };
+  // const nextSlide = () => {
+  //   setSlideCount(slideCount === data.length - 1 ? 0 : slideCount + 1);
+  // };
+  // const previousSlide = () => {
+  //   setSlideCount(slideCount === 0 ? data.length - 1 : slideCount - 1);
+  // };
   return (
-    <div className={styles.carousel}>
-      <div className={styles.slides}>
-        <BsFillArrowLeftCircleFill
+    <div className={styles.embla} ref={emblaRef}>
+      <div className={styles.embla__container}>
+        {/* <BsFillArrowLeftCircleFill
           className={`${styles.arrow} ${styles.leftArrow}`}
           onClick={previousSlide}
-        />
-        {data.map((slide, index) => {
+        /> */}
+        {/* {data.map((slide, index) => {
           return (
             <img
               src={slide.image}
               key={index}
+              height="250"
               className={slideCount === index ? styles.slide : styles.hidden}
             />
           );
-        })}
-        <BsFillArrowRightCircleFill
+        })} */}
+        {/* <BsFillArrowRightCircleFill
           className={`${styles.arrow} ${styles.rightArrow}`}
           onClick={nextSlide}
-        />
+        /> */}
 
-      
+        <div className={styles.embla__slide}>
+          <img src={product1} alt="" />
+        </div>
+        <div className={styles.embla__slide}>
+          <img src={product2} alt="" />
+        </div>
+        <div className={styles.embla__slide}>
+          <img src={product3} alt="" />
+        </div>
       </div>
     </div>
   );
