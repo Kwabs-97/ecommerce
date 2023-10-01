6f9107f12a34818d251933eb525945c15ee6489f
Kwabs-97 <khobewayne@gmail.com>
2023-10-01 05:25:58 +0000

    modal in addition to cart background

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -3,23 +3,24 @@
 import { productsData } from "../Data/Products";
 import CartItem from "./CartItem";
 import styles from "./Cart.module.css";
+import Modal from "../UI/Modal";
 
 const Cart = () => {
- 
-
   return (
-    <div className={styles.cartCont}>
-      {productsData.map((product) => (
-        <CartItem
-          key={product.id}
-          img={product.img}
-          name={product.name}
-          price={product.price}
-          discountPrice={product.discountPrice}
-          quantity={product.quantity}
-        />
-      ))}
-    </div>
+    <Modal>
+     
+        {productsData.map((product) => (
+          <CartItem
+            key={product.id}
+            img={product.img}
+            name={product.name}
+            price={product.price}
+            discountPrice={product.discountPrice}
+            quantity={product.quantity}
+          />
+        ))}
+ 
+    </Modal>
   );
 };
 
